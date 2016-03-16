046d918720ffcd8a90f8aae3361d7a4cfbdcb71a
Maxly Garcia <maxgarcia6@gmail.com>
2016-03-16 19:41:12 -0400

    Desarrolle los metodos principales de la capa negocios y de la capa data declare los metodos e hice su respectivo constructor. Ademas añadí la libreria System.Data

---
+++ SCE/BusinessLayer/BCategory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using System.Data;
+
+namespace BusinessLayer
+{
+    public class BCategory
+    {
+        /// <summary>
+        /// Este método llama al método insertar de la clase "DCategory" del proyecto DataLayer
+        /// </summary>
+        /// <param name="_name"></param>
+        /// <param name="_description"></param>
+        /// <returns>obj</returns>
+        public static string Insert(string _name, string _description)
+        {
+            DCategory obj = new DCategory();
+            obj.Name = _name;
+            obj.Description = _description;
+            return obj.Insert(obj);
+        }
+
+        /// <summary>
+        /// Este método llama al método editar de la clase "DCategory" del proyecto DataLayer
+        /// </summary>
+        /// <param name="_idcategory"></param>
+        /// <param name="_name"></param>
+        /// <param name="_description"></param>
+        /// <returns>obj</returns>
+        public static string Edit(int _idcategory, string _name, string _description)
+        {
+            DCategory obj = new DCategory();
+            obj.IdCategory = _idcategory; 
+            obj.Name = _name;
+            obj.Description = _description;
+            return obj.Edit(obj);
+        }
+
+        /// <summary>
+        /// Este método llama al método eliminar de la clase "DCategory" del proyecto DataLayer
+        /// </summary>
+        /// <param name="_idcategory"></param>
+        /// <returns>obj</returns>
+        public static string Remove(int _idcategory)
+        {
+            DCategory obj = new DCategory();
+            obj.IdCategory = _idcategory;
+            return obj.remove(obj);
+        }
+
+        /// <summary>
+        /// Este método llama al método show de la clase "DCategory" del proyecto DataLayer
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable Show()
+        {
+            return new DCategory().Show();
+        }
+
+        public static DataTable SearchName(string _sText)
+        {
+            DCategory obj = new DCategory();
+            obj.SText = _sText;
+            return obj.SearchName(obj);
+        }
+    }
+}
---
+++ SCE/DataLayer/DCategory.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DataLayer
+{
+    public class DCategory
+    {
+        //Ten en cuenta que los atributos empiezan con minuscula
+        private int idCategory;
+        private string name;
+        private string description;
+        private string sText;
+
+        //Y los metodos con mayuscula. Cambialo si quieres.
+
+        /// <summary>
+        /// Getters y Setters
+        /// </summary>
+        /// 
+        public int IdCategory
+        {
+            get{ return idCategory; }
+            set { idCategory = value; }         
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string SText
+        {
+            get { return sText; }
+            set { sText = value; }
+        }
+        
+
+        /// <summary>
+        /// El constructor llama la funcion del objeto y usa el metodo get o set del cual se
+        /// haga referencia para asignar o recuperar los atributos
+        /// </summary>
+        /// <param name="_idCategory"></param>
+        /// <param name="_name"></param>
+        /// <param name="_description"></param>
+        /// <param name="_sText"></param>
+        public DCategory(int _idCategory, string _name, string _description, string _sText)     //Para los parametros pongo "_" delante del nombre del atributo que se modificara
+        {
+            this.IdCategory = _idCategory;
+            this.Name = _name;
+            this.Description = _description;
+            this.SText = _sText;
+        }
+
+        public DCategory()
+        {
+        }
+
+        /// <summary>
+        /// Método insertar
+        /// </summary>
+        /// <param name="_category"></param>
+        /// <returns></returns>
+        public string Insert(DCategory _category)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Método editar
+        /// </summary>
+        /// <param name="_category"></param>
+        /// <returns></returns>
+        public string Edit(DCategory _category)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Método remover
+        /// </summary>
+        /// <param name="_category"></param>
+        /// <returns></returns>
+        public string remove(DCategory _category)
+        {
+            return null;
+        }
+
+        public DataTable Show()
+        {
+            return null;
+        }
+
+        public DataTable SearchName(DCategory _category)
+        {
+            return null;
+        }
+
+    }
+}
