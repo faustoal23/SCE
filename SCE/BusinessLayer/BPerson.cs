7926582fdeb30b19be9ebbb770d281f256f34c0b
faustoal23 <faustoal23@gmail.com>
2016-03-20 12:57:49 +0800

    Se creo la conexion con la base de datos a nivel de Personas. Cambios en las Capas Datos, Negocio y Presentacion. Formulario parcial de busqueda de personas en la base de datos.

---
+++ SCE/BusinessLayer/BPerson.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataLayer;
+using System.Data;
+
+namespace BusinessLayer
+{
+    public class BPerson
+    {
+        // Metodo Insert, llama al metodo Insert de la capa de datos
+        public static string Insert(string firstname, string surname, string lastname, string sex, string documenttype, string documentnumber, string address)
+        {
+            DPerson Obj = new DPerson();
+            Obj.FirstName = firstname;
+            Obj.SurName = surname;
+            Obj.LastName = lastname;
+            Obj.Sex = sex;
+            Obj.DocumentType = documenttype;
+            Obj.DocumentNumber = documentnumber;
+            Obj.Address = address;
+
+            return Obj.Insert(Obj);
+        }
+
+        // Metodo Editar
+        public static string Edit(int idperson, string firstname, string surname, string lastname, string sex, string documenttype, string documentnumber, string address)
+        {
+            DPerson Obj = new DPerson();
+            Obj.IdPerson = idperson;
+            Obj.FirstName = firstname;
+            Obj.SurName = surname;
+            Obj.LastName = lastname;
+            Obj.Sex = sex;
+            Obj.DocumentType = documenttype;
+            Obj.DocumentNumber = documentnumber;
+            Obj.Address = address;
+
+            return Obj.Edit(Obj);
+        }
+
+        // Metodo Eliminar
+        public static string Delete(int idperson, string firstname, string surname, string lastname, string sex, string documenttype, string documentnumber, string address)
+        {
+            DPerson Obj = new DPerson();
+            Obj.IdPerson = idperson;
+
+            return Obj.Delete(Obj);
+        }
+
+        // Metodo mostrar
+        public static DataTable Show()
+        {
+            return new DPerson().Show();
+        }
+
+        // Metodo buscarnombre
+        public static DataTable SearchByName(string searchtext)
+        {
+            DPerson Obj = new DPerson();
+            Obj.SearchText = searchtext;
+
+            return Obj.SearchByName(Obj);
+        }
+
+        // Metodo buscar apellido
+        public static DataTable SearchByLastName(string searchtext)
+        {
+            DPerson Obj = new DPerson();
+            Obj.SearchText = searchtext;
+
+            return Obj.SearchByLastName(Obj);
+        }
+
+    }
+}
